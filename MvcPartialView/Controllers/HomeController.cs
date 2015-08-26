9c9a9443030206aed38276b38c0b9dfe8500dbdb
raveendrahg <ravindra.hg@gmail.com>
2015-08-26 20:16:43 +0530

    MVC - partial view demo

---
+++ MvcPartialView/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcPartialView.Models;
+
+namespace MvcPartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>() {
+                new Product() { Id = 1, Name = "Product 1", Location = "J P Nagar" } ,
+                new Product() { Id = 2, Name = "Product 2", Location = "Jayanagar" } 
+            };
+            return View(products);
+        }
+
+        public ActionResult _Product()
+        {
+            return View(new Client() { Name = "Client One", Location = "Client Location" });
+        }
+
+    }
+}
---
+++ MvcPartialView/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcPartialView.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+    }
+}
